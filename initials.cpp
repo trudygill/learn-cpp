484ed917b1ccc2468bee849458f79a9d2a40f399
Trudy Gill <trudzzg@gmail.com>
2026-09-23 20:27:23 -0400

    Add initials.cpp to display initials in ASCII art

---
+++ initials.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+// Trudy 
+// I love reading, listening to music and sleeping.
+int main () {
+
+std::cout << "TTTTT   GGGGGGG\n";
+std::cout << "  T     G      \n";
+std::cout << "  T     G      \n";
+std::cout << "  T     G   G G\n";
+std::cout << "  T     G     G\n";
+std::cout << "  T     G     G\n";
+std::cout << "  T     GGGGGGG\n";
+return 0;
+
+}
